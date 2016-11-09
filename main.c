957124ced2fee50563d26ea4dd54071a6ad52fb6
root <root@linux-u32x.site>
2016-11-09 12:32:51 +0100

    Weights implementet. get weights alogorithm to do next

--- main.c
+++ main.c
@@ -2,30 +2,53 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+//Strukturen & Typdefinitionen
+struct wgts{
+	int two_kg;
+	int three_kg;
+	int five_kg;
+};
+typedef struct wgts WEIGHTS;
+
 //Globale Variablen
 int philo_zaehler;
+WEIGHTS depot;
 
 
 // Philosophen - Threadfunktion
 void* philosoph(void* value) {
 
 	philo_zaehler++;
-	printf("Ich bin Philosoph Nr.: %d\n\n", philo_zaehler);
+	printf("Ich bin Philosoph Nr.: %d\n Ich traniere mit %d kg \n", philo_zaehler, (int) value);
 
    return NULL;
 }
 
+//Funktionen
+WEIGHTS GET_WEIGHTS(int kg){
+
+	WEIGHTS output;
+
+	return output;
+}
+
+
 
 int main() {
    
+	// Gewichtespeicher
+   depot.two_kg = 4;
+   depot.three_kg = 4;
+   depot.five_kg = 5;
+
    pthread_t p1, p2, p3, p4, p5;
 
    //starte Threads
-   pthread_create(&p1, NULL, philosoph, NULL);
-   pthread_create(&p2, NULL, philosoph, NULL);
-   pthread_create(&p3, NULL, philosoph, NULL);
-   pthread_create(&p4, NULL, philosoph, NULL);
-   pthread_create(&p5, NULL, philosoph, NULL);
+   pthread_create(&p1, NULL, philosoph, 6);
+   pthread_create(&p2, NULL, philosoph, 8);
+   pthread_create(&p3, NULL, philosoph, 12);
+   pthread_create(&p4, NULL, philosoph, 12);
+   pthread_create(&p5, NULL, philosoph, 14);
 
    //warte auf Threads
    pthread_join(p1, NULL);
