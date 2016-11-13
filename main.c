77b70683083f87c2757ad46ed820ab56ed232f0e
root <root@linux-u32x.site>
2016-11-13 13:02:19 +0100

    first runable version without sychronization. quitable. sync and more input possibilities to do

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
+#define REST_LOOP 1000000000
+#define WORKOUT_LOOP 500000000
 
 //Strukturen & Typdefinitionen
 struct wgts{
@@ -13,8 +16,16 @@ typedef struct wgts WEIGHTS;
 //Globale Variablen
 int philo_zaehler;
 WEIGHTS depot;
+char command[5];
 
 //Funktionen
+
+void PUT_WEIGHTS(WEIGHTS weight){
+	depot.five_kg += weight.five_kg;
+	depot.three_kg += weight.three_kg;
+	depot.two_kg += weight.two_kg;
+}
+
 WEIGHTS GET_WEIGHTS(int value ){
 
 	WEIGHTS output;
@@ -26,6 +37,7 @@ WEIGHTS GET_WEIGHTS(int value ){
 	int modulo;
 	int weight;
 	int sum;
+	
 
 	
 
@@ -107,7 +119,9 @@ WEIGHTS GET_WEIGHTS(int value ){
 	}
 
 	if(sum==value){
-		depot = depot_clone;
+		depot.five_kg  -= output.five_kg;
+		depot.three_kg -= output.three_kg;
+		depot.two_kg   -= output.two_kg;
 	}
 	else{
 		output.five_kg = 0;
@@ -123,14 +137,30 @@ void* philosoph(void* value) {
 
 	philo_zaehler++;
 	
+	int num = philo_zaehler;
 	WEIGHTS myWeights;
+	int i;
+	int loop = 1;
 
-	myWeights = GET_WEIGHTS( (int) value );
+	while(strcmp(command, "q") != 0){
+		myWeights = GET_WEIGHTS( (int) value );
 
-	printf("Ich bin Philosoph Nr.: %d\n Ich traniere mit %d kg (%d, %d, %d)\n", 
-		philo_zaehler, (int) value, myWeights.five_kg, myWeights.three_kg, myWeights.two_kg);
-	printf("-->>Das Depot hat noch: (%d, %d, %d)\n\n", depot.five_kg, depot.three_kg, depot.two_kg);
+		printf("\nIch bin Philosoph Nr.: %d\n Ich traniere mit %d kg (%d, %d, %d)\n", 
+			num, (int) value, myWeights.five_kg, myWeights.three_kg, myWeights.two_kg);
+		printf("-->>Das Depot hat noch: (%d, %d, %d)\n", depot.five_kg, depot.three_kg, depot.two_kg);
 
+
+		for(i=0; i<WORKOUT_LOOP; i++){
+
+		}
+
+		PUT_WEIGHTS(myWeights);
+		printf("\n(%d)Das Depot hat wieder: (%d, %d, %d)\n\n", num, depot.five_kg, depot.three_kg, depot.two_kg);
+		for(i=0; i<REST_LOOP; i++){
+
+		}
+
+	}
 
 	pthread_exit(NULL);
 
@@ -145,10 +175,16 @@ int main() {
    depot.three_kg = 4;
    depot.five_kg = 5;
 
-   // WEIGHTS myWeights = GET_WEIGHTS(12);
+   int run = 1;
 
    // printf("philosoph: (%d, %d, %d)\n", myWeights.five_kg, myWeights.three_kg, myWeights.two_kg);
    // printf("depot: (%d, %d, %d)\n\n", depot.five_kg, depot.three_kg, depot.two_kg);
+
+   // PUT_WEIGHTS( myWeights );
+
+   // printf("philosoph: (%d, %d, %d)\n", myWeights.five_kg, myWeights.three_kg, myWeights.two_kg);
+   // printf("depot: (%d, %d, %d)\n\n", depot.five_kg, depot.three_kg, depot.two_kg);
+
 
    pthread_t p1, p2, p3, p4, p5;
 
@@ -159,20 +195,33 @@ int main() {
    pthread_create(&p4, NULL, philosoph, 12);
    pthread_create(&p5, NULL, philosoph, 14);
 
-   int i;
-   for(i=0; i<10000000; i++){
-   		if(i%1000000==0){
-   			printf("Das Depot hat noch: (%d, %d, %d)\n\n", depot.five_kg, depot.three_kg, depot.two_kg);
-   		}
 
+   while(run){
+ 		fgets(command, 255, stdin);
+ 		//delete new line from input
+ 		int l = strlen(command);
+ 		command[l-1] = '\0';
+ 		
+ 		if(strcmp(command, "q") == 0 ){
+ 			run = 0;
+ 			printf("enter\n");
+ 		}
 
+ 		printf("Befehl: \"%s\" \n", command);
    }
+
+   printf("Programm Ende\n");
+
    // warte auf Threads
-   // pthread_join(p1, NULL);
-   // pthread_join(p2, NULL);
-   // pthread_join(p3, NULL);
-   // pthread_join(p4, NULL);
-   // pthread_join(p5, NULL);
+   pthread_join(p1, NULL);
+   pthread_join(p2, NULL);
+
+   
+   pthread_join(p3, NULL);
+   pthread_join(p4, NULL);
+   pthread_join(p5, NULL);
+
+   printf("depot: (%d, %d, %d)\n\n", depot.five_kg, depot.three_kg, depot.two_kg);
 
    return 0;
 }
